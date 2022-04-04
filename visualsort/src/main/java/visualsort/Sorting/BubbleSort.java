3db4285bef51de8181987ffa8a5f241cf8d52640
corey <camendrella@protonmail.com>
2022-04-04 13:58:51 -0700

    Implemented Bubble Sort algorithm

---
+++ visualsort/src/main/java/visualsort/Sorting/BubbleSort.java
@@ -0,0 +1,67 @@
+package visualsort.Sorting;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BubbleSort {
+
+    private String[] iterations;
+    private int[][] iteration_values;
+
+    public BubbleSort(){
+
+        iterations = new String[10];
+        iteration_values = new int[10][10];
+
+    }
+
+    public void sort(ArrayList < Integer > array, int byWhat) {
+
+        bubbleSort(array);
+
+    }
+
+
+    private void bubbleSort(ArrayList < Integer > array) {
+
+        int size = array.size();
+
+        for (int i = 0; i < size; i++) { 
+
+            iterations(array, i);
+
+            for(int j = 0; j < size - i - 1; j++){
+                if(array.get(j) > array.get(j+1)){
+                    swap(array, j, j+1);
+                }
+            }
+        }
+    }
+
+    private void swap(ArrayList<Integer> array, int a, int b){
+
+        int temp = array.get(a);
+        array.set(a, array.get(b));
+        array.set(b, temp);
+
+    }
+
+    private void iterations(ArrayList<Integer> array, int index){
+
+        iterations[index] = array.toString();
+        updateRectangleArray(array, index);
+
+    }
+
+    private void updateRectangleArray(ArrayList<Integer> arr, int row) {
+
+        int col = 0;
+        Iterator <Integer> itr = arr.iterator();
+
+        while (itr.hasNext()) {
+            iteration_values[row][col] = itr.next();
+            col++;
+        }
+
+    }    
+}
